59dc2cf3b326e7f81e4442212446a7ef30faf426
SerhiiSa10 <serhii.salo@nure.ua>
2024-12-12 15:05:28 +0200

    Moving circle was added

--- lessons/less1_draw.py
+++ lessons/less1_draw.py
@@ -2,6 +2,8 @@ import pygame
 import sys
 from random import randint
 
+import pygame.draw
+
 # Ініціалізація
 pygame.init()
 
@@ -9,10 +11,15 @@ screen_width, screen_height = 800, 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("PyGame TBS v0.1")
 
-for i in range(1, 6):
+FPS = 60
+
+'''for i in range(1, 6):
     color = randint(0,255), randint(0,255), randint(0,255)
     circ = pygame.draw.circle(screen, color, (i*50, i*50), i*5) # намалювати коло
-    print(circ.center)
+    print(circ.center)'''
+
+clock = pygame.time.Clock() # створюємо об'єкт для взаємодії з часом
+circle_x, circle_y = 300, 300
 
 #Основний цикл програми
 runnig = True
@@ -21,7 +28,16 @@ while runnig:
         if event.type == pygame.QUIT:
             runnig = False 
 
+
+    screen.fill((0, 0, 0))
+
+    circle_x += 1
+    circle_y += 1
+
+    pygame.draw.circle(screen, (255, 255, 255), (circle_x, circle_y), 30)
+
     pygame.display.flip() # Малюємо наступний кадр
+    clock.tick(FPS)
 
 
 #Завершення програми
