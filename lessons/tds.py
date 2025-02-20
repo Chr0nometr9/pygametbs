d0f7bd0c0faf52fbcf8a3606c0fff5b85626f099
artChistyakov <tomsonone08@gmail.com>
2025-02-20 14:10:32 +0200

    Added bullet sprite and hero hp

--- lessons/tds.py
+++ lessons/tds.py
@@ -3,15 +3,21 @@ import sys
 import random
 
 class Bullet():
-    def __init__(self, x, y, mouse_x, mouse_y):
+    def __init__(self, x, y, dir_vector):
         self.x = x
         self.y = y
-        self.dir = pygame.math.Vector2(mouse_x - x, mouse_y - y)
-        self.dir.scale_to_length(5)
-        self.bullet_rect = pygame.draw.circle(screen, (0, 0, 255), (self.x, self.y), 5)
+        self.dir = dir_vector.copy()
+        self.dir.scale_to_length(4)
+        self.bullet_sprite = pygame.image.load("bullet.png")
+        self.bullet_sprite = pygame.transform.scale(self.bullet_sprite, (self.bullet_sprite.get_width() // 1.5, self.bullet_sprite.get_height() // 1.5))
+        self.img = self.bullet_sprite
+        self.bullet_rect = self.img.get_rect()
 
     def draw(self, screen):
-        self.bullet_rect = pygame.draw.circle(screen, (0, 0, 255), (self.x, self.y), 5)
+        self.img = pygame.transform.rotate(self.bullet_sprite, self.dir.angle_to((0, -1)))
+        self.bullet_rect = self.img.get_rect()
+        self.bullet_rect.center = (self.x, self.y)
+        screen.blit(self.img, self.bullet_rect)
 
     def move(self, factor=1):
         self.x += self.dir.x * factor
@@ -94,7 +100,8 @@ class ExplosionAnimation():
 # Ініціалізація
 pygame.init()
 
-screen_width, screen_height = 1366, 768
+screen_width, screen_height = 1920, 1080
+# screen_width, screen_height = 1366, 768
 screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
 pygame.display.set_caption("PyGame TBS v0.1")
 
@@ -114,6 +121,7 @@ move = False
 x_rand = random.randint(5, 795)
 y_rand = random.randint(5, 595)
 ship = pygame.image.load("ship.png")
+rotated_ship = ship
 enemy_img = pygame.image.load("enemy.png")
 big_rect = ship.get_rect()
 small_rect = pygame.draw.circle(screen, (255, 255, 0), (x_rand, y_rand), 5)
@@ -123,8 +131,8 @@ score = 0
 bullets = []
 enemies = []
 expl_animations = []
-
-damage = 20
+hp = 100
+damage = 10
 last_create_enemy = 0
 
 #Основний цикл програми
@@ -143,7 +151,12 @@ while runnig:
             mouse_x, mouse_y = event.pos
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                new_bullet = Bullet(x, y, mouse_x, mouse_y)
+                left_bullet_vector = pygame.math.Vector2(-45, 0).rotate(90 - dir_vector.angle_to((1, 0)))
+                right_bullet_vector = pygame.math.Vector2(45, 0).rotate(90 - dir_vector.angle_to((1, 0)))
+                new_bullet = Bullet(x+left_bullet_vector.x, y+left_bullet_vector.y, dir_vector)
+                new_bullet.move(10)
+                bullets.append(new_bullet)
+                new_bullet = Bullet(x+right_bullet_vector.x, y+right_bullet_vector.y, dir_vector)
                 new_bullet.move(10)
                 bullets.append(new_bullet)
     if big_rect.colliderect(small_rect):
@@ -152,6 +165,7 @@ while runnig:
         y_rand = random.randint(5, 595)
 
     screen_width, screen_height = screen.get_size()
+    hero_mask = pygame.mask.from_surface(rotated_ship)
 
     for bullet in bullets:
         for enemy in enemies:
@@ -162,12 +176,26 @@ while runnig:
                     expl_animations.append(ExplosionAnimation(*enemy.get_rect().center))
                     enemy.x = -10000
 
+    for enemy in enemies:
+        enemy_mask = pygame.mask.from_surface(enemy.img)
+        offset = big_rect.x - enemy.get_rect().x, big_rect.y - enemy.get_rect().y
+        if enemy_mask.overlap(hero_mask, offset):
+            hp -= 20
+            expl_animations.append(ExplosionAnimation(*enemy.get_rect().center))
+            enemy.x = -10000
+
+    if hp <= 0:
+        print("You lose")
+        runnig = False
+
     dir_vector = pygame.math.Vector2(mouse_x - x, mouse_y - y)
     a = dir_vector.copy()
-    a.scale_to_length(130)
+    a.scale_to_length(200)
 
     if move:
         a_x, a_y = a.xy
+    else:
+        a_x, a_y = 0, 0
 
     v_x += a_x * dt
     v_y += a_y * dt
@@ -185,7 +213,6 @@ while runnig:
     screen.fill((0,0,0))
     scoretext = font.render(str(score), True, (255, 255, 255))
 
-
     for enemy in enemies:
         enemy.move()
         enemy.draw(screen)
@@ -195,6 +222,10 @@ while runnig:
     big_rect = rotated_ship.get_rect()
     big_rect.center = (x, y)
     small_rect = pygame.draw.circle(screen, (255, 255, 0), (x_rand, y_rand), 5)
+
+    hpbar_rect = pygame.Rect((0, 0), (hp * 5, 25))
+    hpbar_rect.center = (screen_width // 2, screen_height - 60)
+    pygame.draw.rect(screen, (255, 0, 0), hpbar_rect)
 
     screen.blit(scoretext, (0, 0))
     screen.blit(rotated_ship, big_rect)
@@ -208,6 +239,7 @@ while runnig:
     
     bullets = [b for b in bullets if not b.out_of_bounce(screen_width, screen_height)]
     expl_animations = [expl for expl in expl_animations if not expl.end]
+
     pygame.display.flip() # Малюємо наступний кадрa
     clock.tick(60)
     
